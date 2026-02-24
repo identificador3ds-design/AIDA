e78e332c56007afc181de1ed1e98fe9969830fea
TCC-AIDA <identificador3ds@gmail.com>
2026-02-24 10:46:56 -0300

    splittext

---
+++ script.js
@@ -0,0 +1,12 @@
+gsap.registerPlugin(SplitText);
+
+
+let split = SplitText.create(".text", {
+    type: "chars, words"
+});
+
+gsap.from(split.chars, {
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.05,
+})
